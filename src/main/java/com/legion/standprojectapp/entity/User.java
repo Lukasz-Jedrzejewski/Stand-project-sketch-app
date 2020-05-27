51a567766b9aef9c653f10cf5f1d6c38618781ae
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-05-27 11:33:26 +0200

    added User entity, repo, service, controller

---
+++ src/main/java/com/legion/standprojectapp/entity/User.java
@@ -0,0 +1,47 @@
+package com.legion.standprojectapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String companyName;
+    private String companyMail;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyMail() {
+        return companyMail;
+    }
+
+    public void setCompanyMail(String companyMail) {
+        this.companyMail = companyMail;
+    }
+
+    public String toHtml() {
+        return "<ul><li>nazwa klienta = " + companyName + "</li><li>mail klienta = " + companyMail + "</li></ul>";
+    }
+}
---
+++ src/main/java/com/legion/standprojectapp/service/UserService.java
@@ -0,0 +1,14 @@
+package com.legion.standprojectapp.service;
+
+import com.legion.standprojectapp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
