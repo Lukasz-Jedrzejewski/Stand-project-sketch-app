55cf8d2ba1a6c898c7404a49366972a697ed3c91
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-09-22 11:25:49 +0200

    VerificationToken class added

---
+++ src/main/java/com/legion/standprojectapp/entity/VerificationToken.java
@@ -0,0 +1,72 @@
+package com.legion.standprojectapp.entity;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class VerificationToken {
+
+    private static final int EXPIRATION = 60 * 24;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+    private Date expiryDate;
+
+    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Timestamp(calendar.getTime().getTime()));
+        calendar.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(calendar.getTime().getTime());
+    }
+
+    public VerificationToken(){}
+
+    public VerificationToken(User user){
+        this.user = user;
+        expiryDate = calculateExpiryDate(EXPIRATION);
+        token = UUID.randomUUID().toString();
+    }
+
+    public static int getEXPIRATION() {
+        return EXPIRATION;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
