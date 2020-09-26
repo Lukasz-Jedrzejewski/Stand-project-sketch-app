3c039e198495ba97ae18aaee528b44914c60f8a6
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-09-26 12:12:27 +0200

    loading company info in about-us view

--- src/main/java/com/legion/standprojectapp/controller/HomePageNavigationController.java
+++ src/main/java/com/legion/standprojectapp/controller/HomePageNavigationController.java
@@ -1,10 +1,21 @@
 package com.legion.standprojectapp.controller;
 
+import com.legion.standprojectapp.entity.CompanyInfo;
+import com.legion.standprojectapp.service.serviceImpl.CompanyInfoServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class HomePageNavigationController {
+
+    private final CompanyInfoServiceImpl companyInfoService;
+
+    public HomePageNavigationController(CompanyInfoServiceImpl companyInfoService) {
+        this.companyInfoService = companyInfoService;
+    }
 
     @GetMapping("/about-company")
     public String aboutCompanyAction () {
@@ -24,5 +35,10 @@ public class HomePageNavigationController {
     @GetMapping("realisations")
     public String realisationsListAction () {
         return "realisations";
+    }
+
+    @ModelAttribute("companyInfo")
+    public List<CompanyInfo> loadCompanyInfo() {
+        return companyInfoService.findAll();
     }
 }
--- src/main/webapp/WEB-INF/views/about-us.jsp
+++ src/main/webapp/WEB-INF/views/about-us.jsp
@@ -1,9 +1,12 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>About company</title>
 </head>
 <body>
-<h1>This is basic company view</h1>
+<c:forEach items="${companyInfo}" var="info">
+    ${info.description}
+</c:forEach>
 </body>
 </html>
