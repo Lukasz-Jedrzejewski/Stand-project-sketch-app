a3363d1dad7c6dd19413d91fa228201231da5bff
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-09-27 09:08:02 +0200

    designer Entity added

---
+++ src/main/java/com/legion/standprojectapp/entity/Designer.java
@@ -0,0 +1,59 @@
+package com.legion.standprojectapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "designer")
+public class Designer {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String name;
+    private String surname;
+    private String description;
+    @OneToOne
+    private File photography;
+
+    public Designer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public File getPhotography() {
+        return photography;
+    }
+
+    public void setPhotography(File photography) {
+        this.photography = photography;
+    }
+}
