7185a536e439d238c4f487eb831824ddb42ede37
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-11-18 11:36:53 +0100

    add basic methods definitions in realisation service

--- src/main/java/com/legion/standprojectapp/service/RealisationService.java
+++ src/main/java/com/legion/standprojectapp/service/RealisationService.java
@@ -1,4 +1,16 @@
 package com.legion.standprojectapp.service;
 
+import com.legion.standprojectapp.entity.Realisation;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface RealisationService {
+
+    void savePic (Realisation realisation) throws IOException;
+    void deletePic (long id) throws IOException;
+    List<Realisation> findAll();
+    Realisation findOne (long id);
+    void setImportant (long id);
+    List<Realisation> findAllImportant();
 }
