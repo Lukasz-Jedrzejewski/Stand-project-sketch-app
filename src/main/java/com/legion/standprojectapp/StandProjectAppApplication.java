99adcc63df41f3a10a182435330c8c9c19066d26
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-12-05 12:24:06 +0100

    add jasyptConfig class

--- src/main/java/com/legion/standprojectapp/StandProjectAppApplication.java
+++ src/main/java/com/legion/standprojectapp/StandProjectAppApplication.java
@@ -1,11 +1,9 @@
 package com.legion.standprojectapp;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class StandProjectAppApplication {
 
     public static void main(String[] args) {
